ed2f6c463add144e44eaf8ffd65cb7ab7b83b390
Corentin Thomasset <33403840+corentinthomasset@users.noreply.github.com>
2022-10-15 21:32:08 -0400

    Installed icon pack

--- src/entry/app.ts
+++ src/entry/app.ts
@@ -1,9 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
 import { createApp } from 'vue';
 import { v4 as uuidv4 } from 'uuid';
+import Unicon from 'vue-unicons';
 import App from '../views/app/App.vue';
 import store from '../store/app';
 import router from '../router/app';
 import '../styles/tailwind.css';
+import '../icons';
 
 const id = uuidv4();
 document.body.insertAdjacentHTML('beforeend', `<div id="lof-${id}"></div>`);
@@ -11,4 +15,5 @@ document.body.insertAdjacentHTML('beforeend', `<div id="lof-${id}"></div>`);
 const app = createApp(App);
 app.use(store);
 app.use(router);
+app.use(Unicon);
 app.mount(`#lof-${id}`);
---
+++ src/icons.ts
@@ -0,0 +1,20 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+import Unicon from 'vue-unicons';
+import {
+  uniAngleDown,
+  uniExternalLinkAlt,
+  uniTimes,
+  uniArrowDown,
+  uniLinkBroken,
+  uniLocationPoint,
+} from 'vue-unicons/dist/icons';
+
+Unicon.add([
+  uniAngleDown,
+  uniExternalLinkAlt,
+  uniTimes,
+  uniArrowDown,
+  uniLinkBroken,
+  uniLocationPoint,
+]);
